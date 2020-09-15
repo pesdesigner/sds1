1fb7f2a381bbd1eb321ef1b70b8153148dd3f01b
pesdesigner <pauloeduardo.dg@outlook.com>
2020-09-14 23:21:16 -0300

    DTO, Service

--- backend/src/main/java/com/graficoeweb/dspesquisa/repositories/GameRepository.java
+++ backend/src/main/java/com/graficoeweb/dspesquisa/repositories/GameRepository.java
@@ -1,9 +1,11 @@
 package com.graficoeweb.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.graficoeweb.dspesquisa.entities.Game;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long>{
 	
 }
---
+++ backend/src/main/java/com/graficoeweb/dspesquisa/services/GameService.java
@@ -0,0 +1,25 @@
+package com.graficoeweb.dspesquisa.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.graficoeweb.dspesquisa.dto.GameDTO;
+import com.graficoeweb.dspesquisa.entities.Game;
+import com.graficoeweb.dspesquisa.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<GameDTO> findAll(){
+		List<Game> list = repository.findAll();
+		return list.stream().map(x -> new GameDTO(x)).collect(Collectors.toList());
+	}
+}
