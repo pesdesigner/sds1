d4e73418a7642210c2dc8c6503ecfe485cf69e06
pesdesigner <pauloeduardo.dg@outlook.com>
2020-09-19 15:43:55 -0300

    Adicionando rotas no React native

---
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Header from '../../components/Header';
+
+const CreateRecord = () => {
+    return (
+     <>
+     <Header />
+
+    <Text>Hellos create</Text>
+     </>   
+      
+    );
+}
+
+export default CreateRecord;
